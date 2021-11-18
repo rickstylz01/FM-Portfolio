ecb67b31349e6f289311eb35298aa7a5473ef995
Ricardo Maya <rickmaya@gmail.com>
2021-11-18 13:28:22 -0600

    add images in publuc folder. import Thumbnail component and fill in properties for componenet filled with project data

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,36 @@
 import React from "react";
+import Thumbnail from "./Thumbnail";
+// import tourystImg from "../../public/images/tourystImg.png";
+import '../App.css';
 
 const Projects = (props) => {
   return(
     <div>
-      <h1>New component</h1>
+      <h1>Projects</h1>
+      <Thumbnail
+        link="/touryst"
+        image="/image/tourystImg"
+        title="Tour-Info Project"
+        role="Back-End Developer"
+      />
+      <Thumbnail
+        link="/todo-app"
+        image="/image/todoImg"
+        title="Todo Application"
+        role="Software Engineer"
+      />
+      <Thumbnail
+        link="/rickstagram"
+        image="/image/rickstagramImg"
+        title="Rickstagram"
+        role="Software Engineer"
+      />
+      <Thumbnail
+        link="/hidden-gems"
+        image="/image/hiddenGemsImg"
+        title="Hidden Gems"
+        role="Software Engineer"
+      />
     </div>
   )
 }
