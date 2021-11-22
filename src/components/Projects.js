40a77d6b67b9c35aede20118fc4c0a67e1c181d3
Ricardo Maya <rickmaya@gmail.com>
2021-11-21 20:23:29 -0600

    create directory for thumbnail and touryst project components.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Thumbnail from "./Thumbnail";
+import Thumbnail from "../Thumbnail/Thumbnail";
 // import tourystImg from "../../public/images/tourystImg.png";
 import '../App.css';
 
