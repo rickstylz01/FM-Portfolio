800d937fcd1e12278cc22642d1d43f8e6baa5ac1
Ricardo Maya <rickmaya@gmail.com>
2021-12-26 16:04:11 -0600

    add footer component to the About me component

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Navigation from "./Navigation";
+import Footer from "./Footer/Footer";
 const About = (props) => {
   return(
     <div>
       <Navigation />
+
       <h1>New component</h1>
+
+      <Footer />
     </div>
   )
 }
