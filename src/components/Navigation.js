7bc4f53c7a01fc001bba583c23427249139e8846
Ricardo Maya <rickmaya@gmail.com>
2021-12-26 18:53:07 -0600

    add fade effect to navbar and footer links

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {Container, Nav, Navbar} from "react-bootstrap";
+import {Fade} from "react-reveal";
 
 const Navigation = () => {
   return (
@@ -12,24 +13,28 @@ const Navigation = () => {
     >
 
       <Container>
+        <Fade right>
+          <Navbar.Brand href="/">
+            <img
+              alt="custom logo"
+              src="/images/blank-logo.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            Rick Maya
+          </Navbar.Brand>
+        </Fade>
 
-        <Navbar.Brand href="/">
-          <img
-            alt="custom logo"
-            src="/images/blank-logo.png"
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-          />{' '}
-          Rick Maya
-        </Navbar.Brand>
 
         <Navbar.Toggle ariaa-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/about">About Me</Nav.Link>
-            <Nav.Link href="/blog">Blog</Nav.Link>
-            <Nav.Link href="/projects">Projects</Nav.Link>
+            <Fade right>
+              <Nav.Link href="/about">About Me</Nav.Link>
+              <Nav.Link href="/blog">Blog</Nav.Link>
+              <Nav.Link href="/projects">Projects</Nav.Link>
+            </Fade>
           </Nav>
         </Navbar.Collapse>
 
