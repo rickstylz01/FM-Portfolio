b1ffb519dfe71dce285dd98c0b98d10a31d9bc60
Ricardo Maya <rickmaya@gmail.com>
2022-01-02 18:33:05 -0600

    restructure About component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -14,33 +14,39 @@ const About = () => {
         <h1 className="pt-5">About Me</h1>
       </Fade>
 
-      <Fade>
-        <div className="background-image">
-            <Image
-              src="../../images/staring-out.jpeg"
-              alt="standing on a mountain looking out"
-              fluid
-            />
-        </div>
-      </Fade>
+      <Container className="mt-5">
+        <Row>
+          <Col className="background-image order-sm-12">
+            <Fade left>
+              <Image
+                src="../../images/staring-out.jpeg"
+                alt="standing on a mountain looking out"
+                thumbnail
+                fluid
+              />
+            </Fade>
+          </Col>
 
-      <Fade left>
-        <div className="aboutMe-container">
-            <Col className="paragraph-one square border border-warning rounded border-2">
-              <Fade>
+          <Col className="order-sm-0">
+            <Fade right>
+              <div className="aboutMe-container">
                 <h2>
                   Hola, my name is Rick.
                 </h2>
-                <p className="journey-paragraph">
+                <p className="intro-paragraph">
                   I enjoy working on creative projects that are both functionally and esthetically notable. My journey to becoming a full-stack developer began with self-taught lessons and resources. I am a graduate of the  "Grow with Google" Scholarship program, where I was taught the fundamentals of front-end web development.  I recently completed a back-end software engineer training program with an organization called Reskill Americans.
                   <br/>
                   <br/>
                   Thanks to RA, I gained the experience of working in a real-world project scenario with a diverse team of other developers to create full-stack applications.
                 </p>
-              </Fade>
-            </Col>
-        </div>
-      </Fade>
+              </div>
+            </Fade>
+          </Col>
+        </Row>
+      </Container>
+
+
+
 
 
       <Container className="achievements">
