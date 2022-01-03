b184e550e9aabb564c564bdbc8e1b451178b06ca
Ricardo Maya <rickmaya@gmail.com>
2022-01-02 23:53:38 -0600

    remove Row component in AboutMe to make flex-direction possible

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navigation from "../Navigation";
 import Footer from "../Footer/Footer";
 import Fade from "react-reveal";
-import {Col, Container, Image, Row} from "react-bootstrap";
+import {Col, Container, Image} from "react-bootstrap";
 import "./AboutMe.css";
 
 const AboutMe = () => {
@@ -14,35 +14,32 @@ const AboutMe = () => {
         <h1>About Me</h1>
       </Fade>
 
-      <Container className="mt-5">
-        <Row>
-          <Col className="staring-image order-sm-12">
-            <Fade left>
-              <Image
-                src="../../images/staring-out.jpeg"
-                alt="standing on a mountain looking out"
-                thumbnail
-                fluid
-              />
-            </Fade>
-          </Col>
+      <Container className="introduction mt-5">
+        <Col className="staring-image order-sm-12">
+          <Fade left>
+            <Image
+              src="../../images/staring-out.jpeg"
+              alt="standing on a mountain looking out"
+              thumbnail
+              fluid
+            />
+          </Fade>
+        </Col>
 
-          <Col className="aboutMe-container order-sm-1">
-            <Fade right>
-              <p className="intro-paragraph">
-                I enjoy working on creative projects that are both functionally and esthetically notable. My journey to becoming a full-stack developer began with self-taught lessons and resources. I am a graduate of the  "Grow with Google" Scholarship program, where I was taught the fundamentals of front-end web development.  I recently completed a back-end software engineer training program with an organization called Reskill Americans.
-                <br/>
-                <br/>
-                Thanks to RA, I gained the experience of working in a real-world project scenario with a diverse team of other developers to create full-stack applications.
-              </p>
-            </Fade>
-          </Col>
-        </Row>
+        <Col className="aboutMe-container order-sm-1">
+          <Fade right>
+            <p className="intro-paragraph">
+              I enjoy working on creative projects that are both functionally and esthetically notable. My journey to becoming a full-stack developer began with self-taught lessons and resources. I am a graduate of the  "Grow with Google" Scholarship program, where I was taught the fundamentals of front-end web development.  I recently completed a back-end software engineer training program with an organization called Reskill Americans.
+              <br/>
+              <br/>
+              Thanks to RA, I gained the experience of working in a real-world project scenario with a diverse team of other developers to create full-stack applications.
+            </p>
+          </Fade>
+        </Col>
       </Container>
 
       <Fade bottom>
         <div className="achievements">
-          <Row className="mt-5">
             <Col className="google-scholarship">
               <Image
                 src="../../images/GoogleScholarship.jpg"
@@ -59,7 +56,6 @@ const AboutMe = () => {
                 thumbnail
               />
             </Col>
-          </Row>
         </div>
 
       </Fade>
