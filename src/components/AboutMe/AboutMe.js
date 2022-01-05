c6cf61b70c7f6a2f496c9119eb196466b954ddc6
Ricardo Maya <rickmaya@gmail.com>
2022-01-04 21:20:02 -0600

    add bboy image for hobbies and fade effect

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -10,11 +10,12 @@ const AboutMe = () => {
     <div className="AboutMe">
       <Navigation />
 
+      {/*--Introduction--*/}
       <Fade>
         <h1>About Me</h1>
       </Fade>
 
-      <Container className="introduction mt-5">
+      <Container className="flex-display mt-5">
         <Col className="staring-image order-sm-12">
           <Fade left>
             <Image
@@ -28,7 +29,7 @@ const AboutMe = () => {
 
         <Col className="paragraph-container order-sm-1">
           <Fade right>
-            <p className="paragraph-padding">
+            <p className="paragraph-padding text-lrg">
               I enjoy working on creative projects that are both functionally and esthetically notable. My journey to becoming a full-stack developer began with self-taught lessons and resources. I am a graduate of the  "Grow with Google" Scholarship program, where I was taught the fundamentals of front-end web development.  I recently completed a back-end software engineer training program with an organization called Reskill Americans.
               <br/>
               <br/>
@@ -71,12 +72,29 @@ const AboutMe = () => {
         <h2 className="center-text mt-4">Hobbies</h2>
       </div>
 
-      <Container>
-        <p>
-          When I'm not working on projects, I work part time at an after-school non-for-profit program teaching stem classes like LEGO architecture and even an intro to coding class.  Otherwise I'm probably looking for a new hiking trail, or a new restaurant to try out.  I try to keep it balanced.
-        </p>
-      </Container>
-
+      <Fade bottom>
+        <Container className="mt-5">
+          <div className="flex-display">
+            <Col>
+              <p className="paragraph-padding text-x-lrg">
+                When I'm not working on projects, I work part time at an after-school non-for-profit program teaching stem classes like LEGO architecture or even an intro to coding class.
+                <br/>
+                <br/>
+                Otherwise I'm probably looking for a new hiking trail, or a new restaurant to try out.  I try to keep it balanced.
+              </p>
+            </Col>
+            <Col>
+              <Image
+                className="bboy-freeze"
+                src="../../images/bboy.JPG"
+                alt="breakdancing freeze"
+                fluid
+                thumbnail
+              />
+            </Col>
+          </div>
+        </Container>
+      </Fade>
       <Footer />
     </div>
   )
