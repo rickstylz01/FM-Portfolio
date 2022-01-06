9204762ee89259fb51e7013e67b5b6989a685ebc
Ricardo Maya <rickmaya@gmail.com>
2022-01-05 20:47:07 -0600

    changed height of phantom footer from 60 to 40px

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import './Form.css';
-import {Container} from "react-bootstrap";
 
 const Form = () => {
   return (
-    <Container id="contact">
-      <div className="item">
+    <div id="contact">
+      <div>
+        <h2 className="heading mt-4">Reach out!</h2>
+      </div>
+
+      <div>
         <form action="https://formspree.io/f/mvolgdao" method="POST">
           <label className="lbl-name" htmlFor="user-name">Name:</label>
-          <input type="text" id="user-name" name="name" placeholder="Enter your full name" required/>
+          <input type="text" id="user-name" name="name" placeholder="full name" required/>
 
           <label htmlFor="user-email">Email:</label>
           <input id="user-email" type="email" name="email" placeholder="example@email.com" required/>
 
           <label htmlFor="user-message">Message:</label>
-          <textarea id="user-message" name="message" placeholder="Please leave a message" required></textarea>
+          <textarea id="user-message" name="message" placeholder="message" required></textarea>
           <button type="submit">Submit</button>
         </form>
       </div>
-    </Container>
+    </div>
   );
 }
 
